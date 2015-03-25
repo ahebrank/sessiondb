75ebfd26e501133aa2cb134f100bcc09b593c478
Andy Hebrank <ahebrank@gmail.com>
2015-03-25 11:37:16 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Getting EE (extensions) to put PHP native sessions in the database
+
+In some cases (e.g., when load balancing is in use) it may be important to centralize PHP sessions.  EE core doesn't really use them (it uses it's on db-based session management), but some modules do!
+
+## Installation and Use
+
+1. Add this folder to your system/expressionengine/hooks directory
+2. Add this code to system/expressionengine/config/hooks.php:
+
+```
+$hook['pre_controller'] = array(
+  'class' => 'Sessiondb',
+  'function' => 'hook_session',
+  'filepath' => 'hooks/sessiondb',
+  'filename' => 'sessiondb.php');
+```
+3. Change this line in system/expressionengine/config/config.php from `FALSE` to `TRUE`:
+```
+$config['enable_hooks'] = TRUE;
+```
---
+++ sessiondb.php
@@ -0,0 +1,114 @@
+<?php
+
+/*
+=====================================================
+* http://shiflett.org/articles/storing-sessions-in-a-database
+=====================================================
+
+*/
+
+/**
+ * Helper function: use the old-style EE instance for older EE versions
+ */
+
+Class Sessiondb {
+
+  var $table_name = "php_sessions";
+
+
+  function hook_session() {
+    // set up the hooks
+    session_set_save_handler(array($this, '_open'),
+                             array($this, '_close'),
+                             array($this, '_read'),
+                             array($this, '_write'),
+                             array($this, '_destroy'),
+                             array($this, '_clean'));
+  }
+
+  /**
+   * Check if the database table for sessions exists
+   */
+  private function check_db() {
+    if (ee()->db->table_exists($this->table_name)) {
+      return true;
+    }
+    return $this->make_db();
+  }
+
+  /**
+   * Build the session table if it's not there already
+   */
+  private function make_db() {
+    ee()->load->dbforge();
+    $fields = array(
+      'id' => array('type' => 'varchar', 'constraint' => '32'),
+      'access' => array('type' => 'integer', 'constraint' => '10', 'unsigned' => true),
+      'data' => array('type' => 'text')
+      );
+    ee()->dbforge->add_field($fields);
+    ee()->dbforge->add_key('id', true);
+    ee()->dbforge->create_table($this->table_name);
+
+    return true;
+  }
+
+  /**
+   * session_start()
+   */
+  function _open() {
+    $this->check_db();
+  }
+
+  function _close() {
+    // no need to do anything
+  }
+
+  /**
+   * Read session data from the db
+   */
+  function _read($id) {
+    $result = ee()->db->select('data')
+      ->where('id', $id)
+      ->get($this->table_name);
+
+    if (!$result->num_rows()) {
+      return '';
+    }
+
+    return $result->row()->data;
+  }
+
+  /**
+   * Write a session variable
+   */
+  function _write($id, $data) {
+    $access = time();
+
+    $this->_destroy($id);
+    return ee()->db->insert($this->table_name, array(
+      'id' => $id,
+      'data' => $data,
+      'access' => $access
+      ));
+  }
+
+  /**
+   * Delete a session variable
+   */
+  function _destroy($id) {
+    return ee()->db->delete($this->table_name, 
+      array('id' => $id));
+  }
+
+  /**
+   * Clean out old session records
+   */
+  function _clean($max) {
+    $old = time() - $max;
+
+    return ee()->db->delete($this->data_name, 
+      array('access <' => $old));
+  }
+
+}
